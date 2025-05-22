c636a8b61e651f5e12bd6d940dfb7cfe77ee4da0
MICHAEL DARKO <102466680+Mykford@users.noreply.github.com>
2025-05-22 20:42:23 +0000

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,4 +1,5 @@
-const firstArg = process.argv[2];
-const secondArg = process.argv[3];
+const args = process.argv.slice(2)
+// Slicing the process.argv removs the first two values in the array automatically and trims it to only the arguments you need
+// You can then access them with just args[0] and args[1]
 
-console.log(`${firstArg} is ${secondArg}`);
+console.log(args[0] + " is " + args[1])
