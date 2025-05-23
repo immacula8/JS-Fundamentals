ab30aa5cb270832b6d38b6441b028c1fe6b0d0fe
Immaculate <geoffreyimmaculate@gmail.com>
2025-05-23 10:43:14 +0100

    Add some changes to the code

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,13 +1,13 @@
 const messages = [
-    "C is fun",
-    "Python is cool",
-    "JavaScript is amazing"
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
 ];
 
 let output = "";
 
 for (let i = 0; i < messages.length; i++) {
-    output += messages[i] + "\n";
+  output += messages[i] + (i < messages.length - 1 ? "\n" : "");
 }
 
 console.log(output);
