beb7a68ab8bbdc38eeae5eefee7f801dab16ce38
Immaculate <geoffreyimmaculate@gmail.com>
2025-05-23 11:20:26 +0100

    Made changes on the cold

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -4,12 +4,8 @@ if (isNaN(x)) {
   console.log("Missing number of occurrences");
 } else {
   let i = 0;
-  let output = "";
   while (i < x) {
-    output += "C is fun\n";
+    console.log("C is fun"); // Print directly in loop
     i++;
   }
-  // Only print once
-  console.log(output);
 }
-
